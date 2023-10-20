fe28c85e23714f3329f595797985f76941fb880b
Dimitri Schulz Amado <dimitri.s@inatel.br>
2023-10-20 08:49:26 -0300

    Creating and relating State and Country

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from cadastros.models import Cidade
+from cadastros.models import Cidade, Estado, Pais
 
+admin.site.register(Pais)
+admin.site.register(Estado)
 admin.site.register(Cidade)
 
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
@@ -9,7 +10,7 @@ class SidiaBaseListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['titulo'] = ''
+        context['titulo'] = 'INATEL'
         return context
 
 
@@ -40,8 +41,9 @@ class CidadeRegister(CreateView):
     success_url = reverse_lazy('cidades-list')
 
 
-class CidadeUpdate(UpdateView):
+class CidadeUpdate(UpdateView, SuccessMessageMixin):
     model = Cidade
     form_class = CidadeForm
     template_name = 'cadastros/update_cidades.html'
     success_url = reverse_lazy('cidades-list')
+    success_message = 'Success to update register!'
